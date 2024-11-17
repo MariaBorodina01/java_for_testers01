04bca5b2c7231a7f1479ad8c3ce2576e1f1a9051
MariaBorodina <msborodina@gmail.com>
2024-11-17 21:13:19 +0600

    Добавили классы, объекты и тесты

--- sendbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sendbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -1,13 +1,24 @@
 package ru.stqa.geometry.figures;
 
 public class Square {
-    public static void printSquareArea(double side){
+
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public static void printSquareArea(Square s) {
         String text = String.format("Площадь квадрата со стороной %f = %f",
-                side, squareArea(side));
+                s.side, s.area());
         System.out.println(text);
     }
 
-    private static double squareArea(double a) {
-        return a + a;
+    public double area() {
+        return this.side * this.side;
+    }
+
+    public double perimeter() {
+        return 4 * this.side;
     }
 }
